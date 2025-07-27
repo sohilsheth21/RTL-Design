2e4f6ca8b858d87a2dda08e4f0318a5029b8b5d6
Sohil Sheth <sohilsheth21@gmail.com>
2025-07-27 15:53:53 -0500

    solved

---
+++ 01_combinational_logic/01_09_and_gate_using_mux/01_09_and_gate_using_mux.sv
@@ -0,0 +1,36 @@
+//----------------------------------------------------------------------------
+// Example
+//----------------------------------------------------------------------------
+
+module mux
+(
+  input  d0, d1,
+  input  sel,
+  output y
+);
+
+  assign y = sel ? d1 : d0;
+
+endmodule
+
+//----------------------------------------------------------------------------
+// Task
+//----------------------------------------------------------------------------
+
+module and_gate_using_mux
+(
+    input  a,
+    input  b,
+    output o
+);
+
+  // Task:
+  // Implement and gate using instance(s) of mux,
+  // constants 0 and 1, and wire connections
+  mux m1(.d0(0),.d1(1),.sel(a&&b),.y(o));
+  
+  
+  
+
+
+endmodule
---
+++ 01_combinational_logic/01_10_or_gate_using_mux/01_10_or_gate_using_mux.sv
@@ -0,0 +1,34 @@
+//----------------------------------------------------------------------------
+// Example
+//----------------------------------------------------------------------------
+
+module mux
+(
+  input  d0, d1,
+  input  sel,
+  output y
+);
+
+  assign y = sel ? d1 : d0;
+
+endmodule
+
+//----------------------------------------------------------------------------
+// Task
+//----------------------------------------------------------------------------
+
+module or_gate_using_mux
+(
+    input  a,
+    input  b,
+    output o
+);
+
+  // Task:
+
+  // Implement or gate using instance(s) of mux,
+  // constants 0 and 1, and wire connections
+  mux m1(.d0(0),.d1(1),.sel(a||b),.y(o));
+
+
+endmodule
